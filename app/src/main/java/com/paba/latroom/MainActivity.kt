ff390760db1fc0e28a07a306f1b0a89aba0d751d
Bill <136879108+licarin@users.noreply.github.com>
2024-12-10 22:20:59 +0700

    42, still progress

--- app/src/main/java/com/paba/latroom/MainActivity.kt
+++ app/src/main/java/com/paba/latroom/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.paba.latroom.database.daftarBelanjaDB
+import com.paba.latroom.database.daftarbelanja
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -17,6 +20,8 @@ import kotlinx.coroutines.async
 class MainActivity : AppCompatActivity() {
 
     private lateinit var DB: daftarBelanjaDB
+    private lateinit var adapterDaftar: adapterDaftar
+    private var arDaftar: MutableList<daftarbelanja> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +39,12 @@ class MainActivity : AppCompatActivity() {
         _fabAdd.setOnClickListener {
             startActivity(Intent(this, TambahDaftar::class.java))
         }
+
+        adapterDaftar = adapterDaftar(arDaftar)
+
+        var _rvDaftar = findViewById<RecyclerView>(R.id.rvDaftar)
+        _rvDaftar.layoutManager = LinearLayoutManager(this)
+        _rvDaftar.adapter = adapterDaftar
     }
 
     override fun onStart() {
@@ -41,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.Main).async {
             val daftarBelanja = DB.fundaftarBelanjaDAO().selectALL()
             Log.d("data ROOM", daftarBelanja.toString())
+            adapterDaftar.isiData(daftarBelanja)
         }
     }
 }
